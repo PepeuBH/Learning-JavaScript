cd7f3e5e85bc78d77e6385ac8053492af462c20b
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-03-20 10:29:44 -0300

    Upload - variables.js

---
+++ variables.js
@@ -0,0 +1,20 @@
+
+//var allows duplicates variables
+var name = 'Pedro'
+var name = 'João'
+
+//let doesn't
+//  let name = 'Pedro'
+//  let name = 'João' //ERROR
+
+//let is used for local variables
+for(let i = 0; i < 10; i++){ //inside a for loop for example
+    console.log(i)
+}
+
+
+//const is a constant variable, its a read only variable
+const PI = 3.14
+// PI = 8 //ERROR
+console.log(PI)
+
