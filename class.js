f069836f9eea267fa3ab3b1b152d1441e864cfb6
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-03-20 10:30:34 -0300

    Upload - class.js

---
+++ class.js
@@ -0,0 +1,14 @@
+class Worker {
+    constructor(name) {
+        this.name = name
+    }
+
+    get name(){
+        return this.name
+    }
+
+    set name(newName){
+        this.name = newName
+    }
+}
+
