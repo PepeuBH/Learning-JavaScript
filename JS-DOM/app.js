82b1f658c88710d643cd7583b397df06a23c53fd
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-03-20 11:52:39 -0300

    Update - JS-DOM
    Selecting elements in the DOM

---
+++ JS-DOM/app.js
@@ -0,0 +1,23 @@
+//GetElementByClassName()
+const listItem = document.getElementsByClassName('list-items')
+console.log(listItem)
+
+
+//GetElementById()
+const title = document.getElementById('main-heading')
+console.log(title)
+
+
+//getElementsByTagName()
+const tagName = document.getElementsByTagName('li')
+console.log(tagName)
+
+
+//querySelector()
+const container = document.querySelector('div') //select the first element with that name
+console.log(container)
+
+
+//querySelectorAll()
+const containerAll = document.querySelectorAll('div') //select all the elements with that name
+console.log(containerAll)
