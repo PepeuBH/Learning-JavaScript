c0026ab01e684f88865a80884d3f7d6ce05dc1c7
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-03-22 12:19:47 -0300

    Update - Creating element with DOM
    - Created new List Item "Iron Man 3" with DOM
    - Modified CSS and HTML

--- JS-DOM/app.js
+++ JS-DOM/app.js
@@ -2,12 +2,10 @@
 
 
 
-//Styling elements with DOM
-
-
+//styling elements with DOM
 const title = document.querySelector('#main-heading');
-title.style.color = 'green';
-console.log(title)
+title.style.color = 'white';
+
 
 
 //get list-items 
@@ -15,7 +13,24 @@ const listItems = document.querySelectorAll('.list-items')
 
 //the list-items must be in a for loop to apply the effects 
 for(let i = 0; i < listItems.length; i++){
-    listItems[i].style.fontSize = '2rem';
+    listItems[i].style.fontSize = '120%'; //font size is set to 110% in CSS
 }
-console.log(listItems)
+
+
+
+
+//Creating elements with DOM
+
+
+//create li inside the ul
+const ul = document.querySelector('ul')
+const li = document.createElement('li')
+
+//to insert li in ul, use append
+ul.append(li)
+
+//Modify the text inside li
+li.innerText = 'Iron Man 3'
+li.setAttribute('class', 'list-items')
+
 
